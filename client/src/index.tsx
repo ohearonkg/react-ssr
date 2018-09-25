26e14646004658e3147ab5b1ef07fbef363126a9
K.G. O'Hearon <ohearonkg@gmail.com>
2018-09-25 15:08:11 -0300

    creating store with redux saga on both server and client.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import { hydrate } from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
 import Routes from "../../Routes";
 
+import reducer from "./reducers";
+import mySaga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(mySaga);
+
 hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
