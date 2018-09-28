795b8e3ff155561daeacce885a84a77d6fe3546f
K.G. O'Hearon <ohearonkg@gmail.com>
2018-09-28 15:49:03 -0300

    now behaving as desired.

--- client/src/components/Header/Header.spec.tsx
+++ client/src/components/Header/Header.spec.tsx
@@ -17,10 +17,28 @@ describe("Header Component", () => {
   });
 
   /**
-   * Render login button
+   * Render Login button
    */
-  it("should render a login button if its prop value of isAuthenticated is false", () => {
+  it("Should render a login button if its prop value of isAuthenticated is false", () => {
     wrapper.setProps({ isAuthenticated: false });
     expect(wrapper.html()).toMatch("Login");
+  });
+
+  it("Should NOT render a logout button if its prop value of isAuthenticated is false", () => {
+    wrapper.setProps({ isAuthenticated: false });
+    expect(wrapper.html()).not.toMatch("Log Out");
+  });
+
+  /**
+   * Render Logout Button
+   */
+  it("Should render a Logout button if its prop value of isAuthenticated is true", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.html()).toMatch("Log Out");
+  });
+
+  it("Should NOT render a Login button if its prop value of isAuthenticated is true", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.html()).not.toMatch("Login");
   });
 });
