b381d5e28d067722edf2852da6e9a3ead1e73940
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-01 19:53:23 -0300

    starting on the admin's feature.

---
+++ client/src/reducers/Admins/admins.ts
@@ -0,0 +1,67 @@
+import { Switch } from "react-router";
+import { ActionType } from "typesafe-actions";
+import * as adminActions from "../../actions/Admins/admins";
+import {
+  FETCH_ADMINS_FAILURE,
+  FETCH_ADMINS_STARTED,
+  FETCH_ADMINS_SUCCESS
+} from "../../actions/Admins/constants";
+import { IUser } from "../../models/User";
+
+interface IAdminsState {
+  /**
+   * Are we currently loading
+   * the list of admins
+   */
+  loadingAdmins: boolean;
+
+  /**
+   * The list of admins returned
+   * from our api
+   */
+  admins: IUser[];
+
+  /**
+   * Error returned from attempting
+   * to fetch out list of admins
+   */
+  error: Error;
+}
+
+const IAdminsInitialState: IAdminsState = {
+  loadingAdmins: false,
+  admins: null,
+  error: null
+};
+
+type AdminsAction = ActionType<typeof adminActions>;
+
+export default (
+  state: IAdminsState = IAdminsInitialState,
+  action: AdminsAction
+) => {
+  switch (action.type) {
+    case FETCH_ADMINS_STARTED:
+      return {
+        ...state,
+        loadingAdmins: true
+      };
+
+    case FETCH_ADMINS_SUCCESS:
+      return {
+        ...state,
+        loadingAdmins: false,
+        admins: action.payload
+      };
+
+    case FETCH_ADMINS_FAILURE:
+      return {
+        ...state,
+        loadingAdmins: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { StateType } from "typesafe-actions";
+import adminsRedcer from "./Admins/admins";
 import authenticationReducer from "./Authentication/authentication";
 import usersReducer from "./Users/users";
 
 const rootReducer = combineReducers({
+  admins: adminsRedcer,
   users: usersReducer,
   authentication: authenticationReducer
 });
